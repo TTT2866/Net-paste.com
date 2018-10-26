6016827742902fec0c3e5ad1100d0de9d1dd554c
TTT2866 <41979926+TTT2866@users.noreply.github.com>
2018-10-26 21:10:28 +1100

    Update credits.php

--- credits.php
+++ credits.php
@@ -13,6 +13,7 @@ Thanks to the following people for the massive help they provided on the Net-pas
 			  jLynx (https://grabify.link) - Support with errors and my shit scripting
 			  Rushax HF (https://rushax.com) - Suggested things such as having the title as your custom meta if it's set, and adding a logo on the generated paste pages which hyperlink to the homepage 
 			  cgijoe_jhuckaby Reddit (u/cgijoe_jhuckaby) - Fixed the meta example overlay thing for me
+			  Abruptum HF - Found an XSS exploit for me which has been fixed
 			  Sweetalert.js (https://sweetalert.js.org/guides/) - the meta example alert
 			  
 			  You can find the source to this website on Github here - https://github.com/TTT2866/Net-paste.com
